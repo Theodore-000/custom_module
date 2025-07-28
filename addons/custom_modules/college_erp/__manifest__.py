45dbc39c6a815470eeb4016213cd606cbc9405c6
Theodore-000 <teddytez000@gmail.com>
2025-07-28 12:16:30 +0300

    new custom module, college_erp created

---
+++ addons/custom_modules/college_erp/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "College ERP", 
+    "version": "18.0.1.1",
+    "description": "An integrated system for managing colleges",
+    "summary":"College Manager",
+    "category": "Education",
+    "installable": True,
+    "application":True,
+    "sequence": 1,
+    "data": [],
+    "depends": [],
+    "author": "Theodore"
+} 
